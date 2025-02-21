f72fd9eb4b51d70ae363a4b595a582469f5ad1a7
shathi <shathurshika2002@gmail.com>
2025-02-21 12:50:13 -0500

    created new branch for og cv testing

--- startup.sh
+++ startup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-VENV_PATH="/home/grace/Desktop/automatic_dart_scoring/venv"
+VENV_PATH="/home/rpi/Desktop/Capstone/automatic_dart_scoring/venv"
 
 if [ -d "$VENV_PATH" ]; then
     source "$VENV_PATH/bin/activate"
-    export PYTHONPATH="/home/grace/Desktop/automatic_dart_scoring"
+    export PYTHONPATH="/home/rpi/Desktop/Capstone/automatic_dart_scoring"
 
     # Print success messages
     echo "Virtual environment activated."
