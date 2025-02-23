fe91ff0e4a414dc05057b1c394918c622271edac
Shrine101 <lawrencesuitor@gmail.com>
2025-02-23 00:00:58 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,7 +4,7 @@ VENV_PATH="/home/lawrence/Desktop/Grace_Github_pi/g_cap_venv"
 
 if [ -d "$VENV_PATH" ]; then
     source "$VENV_PATH/bin/activate"
-    export PYTHONPATH="/home/grace/Desktop/automatic_dart_scoring"
+    export PYTHONPATH="/home/lawrence/Desktop/Grace_Github_pi/automatic_dart_scoring"
 
     # Print success messages
     echo "Virtual environment activated."
